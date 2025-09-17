2d1a863e2dbb27d71d543b7cc863129fba2d8146
Aien Pahlevan <aienpahlevanhim@gmail.com>
2025-09-17 10:30:10 +0330

    get by is and delete customers api

--- src/Controllers/CustomersController.cs
+++ src/Controllers/CustomersController.cs
@@ -22,6 +22,25 @@ public class CustomersController : ControllerBase
         return Ok(customers);
     }
 
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdCustomer(int id)
+    {
+        Customer customer = await _dbContext.Customer.FirstOrDefaultAsync(c => c.CustomerId == id);
+        if (customer is null)
+        {
+            // do something 
+            return BadRequest();
+        }
+
+        return Ok(new
+        {
+            customer.Username,
+            customer.CreatedAt,
+            customer.Email,
+            customer.WalletBalance,
+        });
+    }
+
     [HttpPost]
     public async Task<IActionResult> PostCustomer([FromBody] PostCustomer body)
     {
@@ -39,6 +58,35 @@ public class CustomersController : ControllerBase
 
         return Ok();
     }
+
+    // [HttpPut("{id}")]
+    // public async Task<IActionResult> PutCustomer(int id)
+    // {
+
+    // }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCustomer(int id)
+    {
+        Customer customer = await _dbContext.Customer.FirstOrDefaultAsync(c => c.CustomerId == id);
+        if (customer is null)
+        {
+            // do something 
+            return BadRequest();
+        }
+        Order order = await _dbContext.Orders.FirstOrDefaultAsync(o => o.CustomerId == id);
+        if (order is null)
+        {
+            // do something 
+            return BadRequest("user has order");
+        }
+
+        _dbContext.Customer.Remove(customer);
+        await _dbContext.SaveChangesAsync();
+
+        return Ok();
+    }
+
 
     // [HttpGet]
     // public async Task<IActionResult> GetCustomers(int page = 1, int pageSize = 10)
